1f148a40e2f544964b0094368e302acc9e51f6da
zenn <72529512+zihwoi@users.noreply.github.com>
2024-10-11 17:44:29 +0800

    set up views and models in login register page

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.user.username
