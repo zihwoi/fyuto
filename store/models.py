b3b5342a9d8a0064394009f879300adbc3035830
zenn <72529512+zihwoi@users.noreply.github.com>
2024-10-11 18:00:42 +0800

    updated views for login and register

--- store/models.py
+++ store/models.py
@@ -11,3 +11,13 @@ class Customer(models.Model):
     
     def __str__(self):
         return self.user.username
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
