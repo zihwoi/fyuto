75e382ff44148f6f6d99fd64f3d0fd410d811a31
zenn <72529512+zihwoi@users.noreply.github.com>
2024-10-15 23:56:26 +0800

    Update models.py

--- store/models.py
+++ store/models.py
@@ -12,12 +12,20 @@ class Customer(models.Model):
     def __str__(self):
         return self.user.username
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='products/')
     stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', default=1)  # Add default
 
     def __str__(self):
         return self.name
