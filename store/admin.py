81290b9411dffd11df3a1c8d01e78551ab5c81b6
zenn <72529512+zihwoi@users.noreply.github.com>
2024-10-16 14:35:11 +0800

    Added category and product for admin

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Product
+from django.utils.html import format_html
+from .models import Product, Category  # Import Product and Category
 
-# Register your models here.
-admin.site.register(Product)
+# Register Product model
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'category', 'image_preview')  # Add image_preview to list_display
+
+    # Custom method to display the product image
+    def image_preview(self, obj):
+        if obj.image:  # Check if the product has an image
+            return format_html('<img src="{}" width="50" height="50" />', obj.image.url)
+        return 'No Image'
+
+    image_preview.short_description = 'Image Preview'  # Set column name in admin
+
+# Register Category model
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)  # Display category name
