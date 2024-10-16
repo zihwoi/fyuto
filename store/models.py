5ffac8131fc33d0a3504f323626d48522f2b8155
zenn <72529512+zihwoi@users.noreply.github.com>
2024-10-16 14:34:45 +0800

    Update models.py

--- store/models.py
+++ store/models.py
@@ -19,6 +19,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Category'  # Singular name
+        verbose_name_plural = 'Categories'  # Plural name
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
